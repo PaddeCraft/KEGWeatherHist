064760c9e84c9c729953c118b1e55b14471a6db7
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2023-11-21 13:21:25 +0100

    Finalize FTP upload

--- start.py
+++ start.py
@@ -1,15 +1,32 @@
+from src.data import history, WeatherData
+from src.meteohub import fmt_data
 from src.build import build_files
 from src.env import env
 from src.ftp import *
 
+import time
+
 from tempfile import TemporaryDirectory
 
-ftp = ftp_connect(env.get("FTP_ADDR"), env.get("FTP_USER"), env.get("FTP_PASS"))
-ensure_directory(ftp, env.get("FTP_DIR"))
 
-with TemporaryDirectory() as tmp_dir:
-    build_files(tmp_dir)
-    upload_directory(ftp, tmp_dir, env.get("FTP_DIR"))
-    print("Done")
+def loop():
+    data, timestamp = fmt_data()
 
-ftp.close()
+    history.load(WeatherData(**data), timestamp)
+
+    ftp = ftp_connect(env.get("FTP_ADDR"), env.get("FTP_USER"), env.get("FTP_PASS"))
+    ensure_directory(ftp, env.get("FTP_DIR"))
+    
+    with TemporaryDirectory() as tmp_dir:
+        build_files(tmp_dir)
+        upload_directory(ftp, tmp_dir, env.get("FTP_DIR"))
+    
+    ftp.close()
+
+next_post = time.time()
+
+while True:
+    if time.time() > next_post:
+        next_post += 60 * env.get("UPLOAD_INTERVAL", 5)
+        loop()
+        time.sleep(1)
