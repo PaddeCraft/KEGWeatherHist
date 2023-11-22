40ac941da51c35fc3ac7ebdd48068665b771d108
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2023-11-22 16:31:47 +0100

    Add SFTP support (Co-authored by @NprogramDev)

--- src/env.py
+++ src/env.py
@@ -4,6 +4,8 @@ typed_vars = [
     v("FTP_USER", str),
     v("FTP_PASS", str),
     v("FTP_ADDR", str),
+    v("USE_SFTP", bool),
+    v("SFTP_KEY", str, OPTIONAL),
     v("FTP_DIR", str),
     v("METEOHUB_URL", str),
     v("TIMEZONE", str, OPTIONAL),
--- src/ftp.py
+++ src/ftp.py
@@ -1,55 +1,131 @@
 import ftplib
+import pysftp
+import paramiko
+
+from base64 import decodebytes
+
 import os
 
 
-def ftp_connect(host, user, passwd):
-    ftp = ftplib.FTP()
+class FTPLib:
+    @staticmethod
+    def get_connector(
+        host, user, passwd, is_sftp, known_hosts=None
+    ) -> "FTPLib.Connector":
+        if is_sftp:
+            return FTPLib.SFTPConnector(host, user, passwd, known_hosts)
+        return FTPLib.FTPConnector(host, user, passwd)
 
-    port = 21
-    if ":" in host:
-        port = int(host.split(":")[1])
-        host = host.split(":")[0]
-
-    ftp.connect(host, port)
-
-    try:
-        ftp.login(user, passwd)
-    except ftplib.error_perm:
-        return None
-
-    return ftp
-
-
-def upload_file(ftp, local_path, remote_path):
-    with open(local_path, "rb") as file_descriptor:
-        try:
-            ftp.storbinary("STOR " + remote_path, file_descriptor)
-        except ftplib.error_perm:
-            return False
-
-    return True
-
-
-def ensure_directory(ftp, path):
-    pth = ""
-    for part in path.split("/"):
-        pth += part + "/"
-        try:
-            ftp.mkd(pth)
-        except ftplib.error_perm as e:
+    class Connector:
+        def __init__(self, host, user, passwd, known_hosts=None) -> None:
             pass
 
+        def upload_file(self, path, remote_path):
+            pass
 
-def upload_directory(ftp, local_path, remote_path):
-    ensure_directory(ftp, remote_path)
+        def upload_directory(self, path, remote_path):
+            pass
 
-    for file in os.listdir(local_path):
-        path = os.path.join(local_path, file)
-        if os.path.isdir(path):
-            upload_directory(ftp, path, os.path.join(remote_path, file))
+        def ensure_directory(self, remote_path):
+            pass
 
-        else:
-            if not upload_file(ftp, path, os.path.join(remote_path, file)):
-                return False
+        def close(self):
+            pass
 
-    return True
+    class FTPConnector(Connector):
+        def __init__(self, host, user, passwd, known_hosts) -> None:
+            ftp = ftplib.FTP()
+
+            port = 21
+            if ":" in host:
+                port = int(host.split(":")[1])
+                host = host.split(":")[0]
+
+            ftp.connect(host, port)
+
+            try:
+                ftp.login(user, passwd)
+            except ftplib.error_perm:
+                raise Exception("Invalid credentials")
+
+            self.ftp = ftp
+
+        def upload_directory(self, path, remote_path):
+            self.ensure_directory(remote_path)
+
+            for file in os.listdir(path):
+                path = os.path.join(path, file)
+                if os.path.isdir(path):
+                    self.upload_directory(remote_path, os.path.join(remote_path, file))
+
+                else:
+                    if not self.upload_file(path, os.path.join(remote_path, file)):
+                        return False
+
+            return True
+
+        def upload_file(self, path, remote_path):
+            with open(path, "rb") as file_descriptor:
+                try:
+                    self.ftp.storbinary("STOR " + remote_path, file_descriptor)
+                except ftplib.error_perm:
+                    return False
+
+            return True
+
+        def ensure_directory(self, remote_path):
+            pth = ""
+
+            for part in remote_path.split("/"):
+                pth += part + "/"
+
+                try:
+                    self.ftp.mkd(pth)
+                except ftplib.error_perm as e:
+                    pass
+
+        def close(self):
+            return self.ftp.close()
+
+    class SFTPConnector(Connector):
+        @staticmethod
+        def get_ssh_key(known_hosts):
+            key = None
+            data = decodebytes(known_hosts.split(" ")[2].encode())
+            match known_hosts.split(" ")[1]:
+                case "ssh-rsa":
+                    key = paramiko.RSAKey(data=data)
+                case "ssh-ed25519":
+                    key = paramiko.Ed25519Key(data=data)
+                case "ecdsa-sha2-nistp256":
+                    key = paramiko.ECDSAKey(data=data)
+                case "ssh-dss":
+                    key = paramiko.DSSKey(data=data)
+                case _:
+                    raise "Invalid key type!"
+
+            return *known_hosts.split(" ")[:2], key
+
+        def __init__(self, host, user, passwd, known_hosts):
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys.add(*FTPLib.SFTPConnector.get_ssh_key(known_hosts))
+
+            self.sftp = pysftp.Connection(
+                host,
+                username=user,
+                password=passwd,
+                cnopts=cnopts,
+            )
+
+        def upload_file(self, path, remote_path):
+            self.sftp.put(path, remote_path)
+
+        def upload_directory(self, path, remote_path):
+            self.sftp.put_r(path, remote_path)
+
+        def ensure_directory(self, remote_path):
+            if not self.sftp.exists(remote_path):
+                self.sftp.makedirs(remote_path)
+
+        def close(self):
+            self.sftp.close()
